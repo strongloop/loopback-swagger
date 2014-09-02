fb2fe11b617c0c111cfb8515e10e4e31152814fd
Raymond Feng <raymond@strongloop.com>
2014-09-02 16:19:12 -0700

    Add more comments

--- index.js
+++ index.js
@@ -14,10 +14,39 @@ function getGenerator(spec) {
   return generator;
 }
 
+/**
+ * Generate remote methods from swagger spec
+ * @param {Object} spec
+ * @param {Object} options
+ * @returns {String}
+ */
 exports.generateRemoteMethods = function(spec, options) {
   return getGenerator(spec).generateRemoteMethods(spec, options);
 };
 
+/**
+ * Generate remote methods for an array of operations
+ * @param {String|Number} version
+ * @param {String} modelName
+ * @param {BaseOperation[]} operations
+ * @returns {String}
+ */
+exports.generateCode = function (version, modelName, operations) {
+  var spec = {};
+  if (version === '1.2') {
+    spec.swaggerVersion = '1.2';
+  } else {
+    spec.swagger = 2;
+  }
+  return getGenerator(spec).generateCodeForOperations(modelName, operations);
+};
+
+/**
+ * Generate model definitions
+ * @param {Object} spec Swagger spec
+ * @param {Object} options
+ * @returns {Object}
+ */
 exports.generateModels = function (spec, options) {
   var models;
   if (spec && spec.swagger === 2) {
