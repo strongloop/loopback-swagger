e0832e3dcd4fdc9805d33829a041216d12797a89
Raymond Feng <enjoyjava@gmail.com>
2019-04-08 17:34:09 -0700

    Allow tag or param names to contain '-' or spaces

--- test/codegen/swagger-v2.test.js
+++ test/codegen/swagger-v2.test.js
@@ -7,6 +7,7 @@
 
 var expect = require('chai').expect;
 var V2Generator = require('../../lib/codegen/generator-v2');
+var generateModels = require('../../index').generateModels;
 
 var petStoreV2Spec = require('../../example/pet-store-2.0.json');
 var pet2 = require('./pet-expanded.json');
@@ -14,6 +15,7 @@ var pet3 = require('./pet-without-tags.json');
 var pet4 = require('./pet-with-embedded-schema.json');
 var note = require('./note.json');
 var pet5 = require('./pet-with-refs.json');
+var pet6 = require('./pet-with-special-names.json');
 var generator = new V2Generator();
 
 describe('Swagger spec v2 generator', function() {
@@ -25,11 +27,26 @@ describe('Swagger spec v2 generator', function() {
 
   it('generates remote methods without tags', function() {
     generator.mapTagsToModels(pet3);
-    var models = require('../../index').generateModels(pet3);
+    var models = generateModels(pet3);
     expect(models.SwaggerModel).to.be.a('object');
     var code = generator.generateRemoteMethods(pet3);
     expect(code.SwaggerModel).to.be.a('string');
     expect(Object.keys(code)).to.eql(['SwaggerModel']);
+  });
+
+  it('generates remote methods with special names', function() {
+    generator.mapTagsToModels(pet6);
+    var models = generateModels(pet6);
+    expect(models.Pet_Controller).to.be.a('object');
+    var code = generator.generateRemoteMethods(pet6);
+    expect(code.Pet_Controller).to.be.a('string');
+    var petController = code.Pet_Controller;
+    expect(petController).to.contain(
+      'Pet_Controller.petFindPets = function(tags, x_limit, callback) {'
+    );
+    expect(petController).to.contain(
+      'Pet.find({limit: x_limit, where: {inq: tags}}, callback);'
+    );
   });
 
   it('parse operations', function() {
